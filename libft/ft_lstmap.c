cd13b7853ece985eaba8264b9bc5488b6aa735cd
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-19 20:16:49 +0900

    modified:prototype declaration

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void*))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void *))
 {
 	t_list *new_list;
 	t_list *new_element;
