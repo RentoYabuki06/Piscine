2a8b1f18650da5ad4ea993004c791f3fd2471fef
Rento Yabuki <ryabuki@c2r6s3.42tokyo.jp>
2024-04-21 18:25:10 +0900

    ft_memmove.c

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,12 +6,24 @@
 /*   By: ryabuki <ryabuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 14:53:55 by yabukirento       #+#    #+#             */
-/*   Updated: 2024/04/21 18:10:33 by ryabuki          ###   ########.fr       */
+/*   Updated: 2024/04/21 18:24:10 by ryabuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stddef.h> 
+#include <stddef.h>
+
+static void	ft_cpy(unsigned char *pdst, const unsigned char *psrc, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < len)
+	{
+		*(pdst++) = *(psrc++);
+		i++;
+	}
+}
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
@@ -28,11 +40,13 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	{
 		pdst += len - 1;
 		psrc += len - 1;
+		while (i < len)
+		{
+			*(pdst--) = *(psrc--);
+			i++;
+		}
 	}
-	while (i < len)
-	{
-		*(pdst++) = *(psrc++);
-		i++;
-	}
+	else
+		ft_cpy(pdst, psrc, len);
 	return (dst);
 }
