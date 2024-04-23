26b63e3403cca2358c2e8bf0b8a3aaf560708c75
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-23 16:17:28 +0900

    separted function

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,11 +6,27 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 14:53:55 by yabukirento       #+#    #+#             */
-/*   Updated: 2024/04/23 16:12:02 by yabukirento      ###   ########.fr       */
+/*   Updated: 2024/04/23 16:16:50 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	ft_sign(const char *str, int *sign)
+{
+	int i;
+
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			*sign = -1;
+		i++;
+	}
+	return (i);
+}
 
 int	ft_atoi(const char *str)
 {
@@ -18,27 +34,12 @@ int	ft_atoi(const char *str)
 	int			sign;
 	long long	ans;
 
-	i = 0;
 	ans = 0;
 	sign = 1;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
-		i++;
-	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			sign = -1;
-		i++;
-	}
+	i = ft_sign(str, &sign);
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if (ans > (INT64_MAX / 10))
-		{
-			if (sign == 1)
-				return (-1);
-			if (sign == -1)
-				return (0);
-		}
-		if (ans == (INT64_MAX / 10) && (str[i] >= '8' && str[i] <= '9'))
+		if ((ans > (INT64_MAX / 10)) || (ans == (INT64_MAX / 10) && (str[i] >= '8' && str[i] <= '9')))
 		{
 			if (sign == 1)
 				return (-1);
