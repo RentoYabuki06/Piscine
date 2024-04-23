8699ae28b4fc50ed89b1ebc594088d834aef8c81
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-23 13:44:54 +0900

    modified:needle is null character

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 14:53:55 by yabukirento       #+#    #+#             */
-/*   Updated: 2024/04/21 19:40:07 by yabukirento      ###   ########.fr       */
+/*   Updated: 2024/04/23 13:44:10 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 	size_t	len_needle;
 
-	if (*haystack == '\0' && *needle == '\0')
+	if (*needle == '\0')
 		return ((char *)haystack);
-	if (len < 1)
-		return (0);
 	len_needle = ft_strlen(needle);
 	i = 0;
 	while (i < len && len - i >= len_needle && *haystack != '\0') 
