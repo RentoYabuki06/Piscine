a2509b1b875698ea51d9605e2619ff6be44ff118
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-23 16:52:08 +0900

    add:len==0

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 14:53:55 by yabukirento       #+#    #+#             */
-/*   Updated: 2024/04/23 14:41:52 by yabukirento      ###   ########.fr       */
+/*   Updated: 2024/04/23 16:46:36 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,20 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	len_needle;
+	size_t	len_haystack;
 	char	*hay;
 
 	i = 0;
+	if (len == 0 && *needle == '\0')
+		return ((char *)haystack);
+	if (len == 0)
+		return (NULL);
 	len_needle = ft_strlen(needle);
+	len_haystack = ft_strlen(haystack);
 	if (*needle == '\0')
 		return ((char *)haystack);
 	hay = (char *)haystack;
-	while (i < len && len - i >= len_needle && *hay != '\0') 
+	while (i < len && len - i >= len_needle && i < len_haystack) 
 	{
 		if (ft_strncmp((hay + i), needle, len_needle) == 0)
 		{
