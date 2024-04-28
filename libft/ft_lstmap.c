5fada66a25746492935c1c5bbadbbf2f35b2d5b2
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-28 11:53:09 +0900

    add:ft_lstclear

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,29 +6,48 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 17:50:33 by ryabuki           #+#    #+#             */
-/*   Updated: 2024/04/23 17:34:25 by yabukirento      ###   ########.fr       */
+/*   Updated: 2024/04/28 11:52:39 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	if (!lst || !del)
+		return ;
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+		del((*lst)->content);
+		free(lst);
+		*lst = tmp;
+	}
+	*lst = NULL;
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
-	t_list	*new_element;
+	t_list	*new_node;
+	void	*tmp;
 
-	new_list = NULL;
 	if (!lst || !f)
 		return (NULL);
+	new_list = NULL;
 	while (lst)
 	{
-		new_element = ft_lstnew(f(lst->content));
-		if (!new_element)
+		tmp = f(lst->content);
+		new_node = ft_lstnew(tmp);
+		if (!new_node)
 		{
 			ft_lstclear(&new_list, del);
+			del(tmp);
 			return (NULL);
 		}
-		ft_lstadd_back(&new_list, new_element);
+		ft_lstadd_back(&new_list, new_node);
 		lst = lst->next;
 	}
 	return (new_list);
