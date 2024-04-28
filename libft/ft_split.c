cd3efe2df0783592b9519827703aa35b0bc404ee
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-28 18:24:50 +0900

    allfree

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,15 +6,11 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 14:53:55 by yabukirento       #+#    #+#             */
-/*   Updated: 2024/04/28 14:44:21 by yabukirento      ###   ########.fr       */
+/*   Updated: 2024/04/28 15:24:40 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stddef.h>
 
 static size_t	ft_count(char const *s, char c)
 {
