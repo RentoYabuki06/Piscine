0ce9b6c0134951eb9748da806a8c639bd4841064
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-28 11:17:34 +0900

    comment delete

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,13 +6,13 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 17:50:33 by ryabuki           #+#    #+#             */
-/*   Updated: 2024/04/21 20:25:39 by yabukirento      ###   ########.fr       */
+/*   Updated: 2024/04/23 17:34:25 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_element;
